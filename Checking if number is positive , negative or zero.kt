279a543b93bc7dfd1c3e7af5d97175ee46e3c5ac
Malik Tayyab <muhammadtayyab7541@gmail.com>
2025-07-23 11:49:07 +0500

    Create Checking if number is positive , negative or zero.kt

---
+++ Checking if number is positive , negative or zero.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>)
+{
+val num:Int = 4
+if(num>0)
+{
+println ("number is positive ")
+}
+else if(num==0)
+{
+println ("number is zero")
+}
+else 
+{
+println ("number is positive negative ")
+}
+}
