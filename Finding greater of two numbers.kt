f3bc9b98d6bf693e854c8b4b2445584d9316e451
Malik Tayyab <muhammadtayyab7541@gmail.com>
2025-07-23 11:46:46 +0500

    Create Finding greater of two numbers.kt

---
+++ Finding greater of two numbers.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>)
+{
+var num1 :Int =8
+var num2 :Int = 9
+if(num1>num2)
+{
+println ("$num1 is greater than $num2")
+}
+else 
+{
+println ("$num2 is greater than $num1")
+}
+}
