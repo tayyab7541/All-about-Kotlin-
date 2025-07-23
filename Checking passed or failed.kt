f27dca7d393e11ea3dd14ffb70f4929148d99f6c
Malik Tayyab <muhammadtayyab7541@gmail.com>
2025-07-23 11:38:50 +0500

    Create Checking passed or failed.kt

---
+++ Checking passed or failed.kt
@@ -0,0 +1,12 @@
+fun main() {
+	print("Enter your marks = ")
+    val marks=readLine()!!.toInt()
+    if(marks>=70)
+    {
+    println("Student passed")
+    }
+    else
+    {
+    println ("Student Failed")
+    }
+}
